516400fe4a0fbcbe9ae705a1917df6a852884689
Matvey <66329687+mavotronik@users.noreply.github.com>
2021-11-07 00:02:43 +0300

    Update BSHOS2.24.ino

--- BSHOS2.24/BSHOS2.24.ino
+++ BSHOS2.24/BSHOS2.24.ino
@@ -98,9 +98,9 @@ void setup()
   // выводим сообщение об удачной инициализации GPRS Shield
   Serial.println("GPRS init success");
 
-  gprs.sendSMS("+79213310104", "SYSTEM: POWER IS ON!");
+  gprs.sendSMS("+7xxxxxxxxxx", "SYSTEM: POWER IS ON!");
   delay(3000);
-  gprs.sendSMS("+79062585121", "SYSTEM: POWER IS ON!");
+  gprs.sendSMS("+7xxxxxxxxxx", "SYSTEM: POWER IS ON!");
   delay(1000);
   aauto(); //Автовосстановление состояния при перезагрузке
 }
